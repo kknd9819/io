1880ec3988f0bc601fa3bc51e74cfed1c706a8f5
kknd9819 <kknd9819@qq.com>
2017-07-22 13:33:40 +0800

    OK

--- src/com/zz/io/Constants.java
+++ src/com/zz/io/Constants.java
@@ -4,7 +4,7 @@ public interface Constants {
 
 //	public final static String RECEIVE_FILE_PATH = "G:\\receive\\";    
     
-    public final static String SEND_FILE_PATH = "E:\\a\\";    
+    public final static String SEND_FILE_PATH = "E:\\";    
         
     public final static int DEFAULT_BIND_PORT = 10000;    
 }
